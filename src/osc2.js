f031d316b467d46585982c08b53b3e1c934fc39c
Ben Engel <benjamin.engel@asam.net>
2021-09-24 16:35:13 +0200

    Initial commit

---
+++ src/osc2.js
@@ -0,0 +1,134 @@
+/*
+Language: ASAM OpenSCENARIO 2.0
+Author: Benjamin Engel <benjamin.engel@asam.net>
+Description: Language definition for the ASAM OpenSCENARIO 2 DSL. 
+Website: asam.net
+*/
+
+import * as regex from "../lib/regex.js";
+
+export default function(hljs) {
+  const TEMPORAL_OPERATORS = [
+    "serial",
+    "parallel",
+    "until",
+    "wait",
+  ];
+
+
+
+  const TYPES = [
+    "int",
+    "uint",
+    "float",
+    "string",
+    "weight",
+    "distance",
+    "time",
+    "current",
+    "temperature",
+    "substance",
+    "luminosity",
+    "angle",
+    "struct",
+    "list",
+    "bool",
+  ];
+
+  const BUILT_INS = [
+    "modifier",
+    "extend",
+  ];
+  const RESERVED_WORDS = [
+    "any",
+    "cover",
+    "do",
+    "import",
+    "type",
+    "event",
+    "actor",
+    "scenario",
+    "emit",
+    "is",
+    "keep",
+    "label",
+    "sample",
+    "soft",
+    "default",
+    "struct",
+    "when",
+    "with",
+    "var",
+  ];
+
+  const LITERALS = ["true", "false"];
+
+  const KEYWORDS = {
+    $pattern: /[a-z]\w+/,
+    keyword: RESERVED_WORDS,
+    built_in: BUILT_INS,
+    literal: LITERALS,
+    type: TYPES,
+    operator: TEMPORAL_OPERATORS,
+  };
+
+
+  const STRING = {
+    className: "string",
+    contains: [hljs.BACKSLASH_ESCAPE],
+    variants: [
+      hljs.APOS_STRING_MODE,
+      hljs.QUOTE_STRING_MODE,
+    ],
+  };
+
+  const NUMBERS = {
+    className: 'number',
+    variants: [
+      {
+        begin: '\\b(0b[01\']+)'
+      },
+      {
+        begin: '(-?)\\b([\\d\']+(\\.[\\d\']*)?|\\.[\\d\']+)((ll|LL|l|L)(u|U)?|(u|U)(ll|LL|l|L)?|f|F|b|B)'
+      },
+      {
+        begin: '(-?)(\\b0[xX][a-fA-F0-9\']+|(\\b[\\d\']+(\\.[\\d\']*)?|\\.[\\d\']+)([eE][-+]?[\\d\']+)?)'
+      }
+    ],
+    relevance: 0
+  };
+
+  const PARAMS = {
+    className: "params",
+    variants: [
+      // Exclude params in functions without params
+      {
+        className: "",
+        begin: /\(\s*\)/,
+        skip: true,
+      },
+      {
+        begin: /\(/,
+        end: /\)/,
+        excludeBegin: true,
+        excludeEnd: true,
+        keywords: KEYWORDS,
+        contains: ["self",  NUMBERS, STRING, hljs.HASH_COMMENT_MODE],
+      },
+    ],
+  };
+
+  return {
+    name: "OpenSCENARIO2",
+    aliases: ["osc2"],
+    keywords: KEYWORDS,
+    contains: [
+      STRING,
+      hljs.HASH_COMMENT_MODE,
+      hljs.C_BLOCK_COMMENT_MODE,
+      NUMBERS,
+      PARAMS,
+      //OPERATORS,
+    ],
+  };
+}
